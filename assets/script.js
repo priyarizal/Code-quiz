eaf26625aad493650519ff1e64b3beea2cee81eb
priyadarshini <priya.rizal2@gmail.com>
2022-06-25 16:14:36 -0400

    CSS

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,45 @@ buildQuiz();
 
 // on submit, show results
 submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+    {
+        question1: "Commonly used data type DO NOT include",
+        answers: {
+            a: "Numbers",
+            b: "Booleans",
+            c: "Alerts",
+            d: "Strings"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question2: "Condition in an if/else statement is enclosed with ",
+        answers: {
+            a: "Quotes",
+            b: "Curly Brackets",
+            c: "Parentheses",
+            d: "Square Brackets"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question3: "A very useful tool used during development and debugging for printing conten to the debugger is:",
+        answers: {
+            a: "Javascript",
+            b: "Terminal Bash",
+            c: "For Loops",
+            d: "Console.log"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question4: "Arrays in javascript can be used to store",
+        answers: {
+            a: "numbers and strings",
+            b: "other arrays",
+            c: "Booleans",
+            d: "All of the above"
+        }
+    }
+];
